f53b0a882b468d2de070828048d5805fe4101fff
Your Name <you@example.com>
2021-07-08 08:52:33 +0530

    sec commit

--- elb.tf
+++ elb.tf
@@ -18,7 +18,6 @@ resource "aws_elb" "bar" {
     interval            = 30
   }
 
-  instances                   = [aws_instance.foo.id]
   cross_zone_load_balancing   = true
   idle_timeout                = 400
   connection_draining         = true
